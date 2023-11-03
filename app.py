deda07206c9d29aaa6755aeddf099e51ff8e32e3
ahmed-zhran <modymoro2015@gmail.com>
2023-11-04 00:17:49 +0200

    updating the prompt to list out the giraffe juice components and updating model configuration

--- app.py
+++ app.py
@@ -11,17 +11,31 @@ app.config.update(SECRET_KEY='878as7d8f7997dfaewrwv8asdf8)(dS&A&*d78(*&ASD08A')
 SESSION_KEY = "json"
 
 def __default_message(message:str):
-    new_prompt = "Rephrase the following in NVC Language. Be careful to distinguish pseudofeelings from feelings. \n" + message
     # new_prompt = "Rephrase in NVC language " + message
+    # new_prompt = "Rephrase the following in NVC Language. Be careful to distinguish pseudofeelings from feelings. \n" + message
+    # new_prompt = "I will give you a text rephrase in NVC Language. Be careful to distinguish pseudofeelings from feelings. Rephrase it and then list down those [Observations, Feelings, Needs,  Requests] after the rephrased text. here is the original text to rephrase: \""+ message + "\". I need the full response in json object including [original_txt, rephrased_txt, Observations, Feelings, Needs,  Requests]"
+    # new_prompt = "I will give you a text rephrase in NVC Language. Be careful to distinguish pseudofeelings from feelings. Rephrase it and then list down those [Observations, Feelings, Needs,  Requests] after the rephrased text. here is the original text to rephrase: \""+ message + "\". I need each of those [original_txt, rephrased_txt, Observations, Feelings, Needs,  Requests] in a seperate line without gab lines or prefix or postfix response explanation or start ."
+    new_prompt = 'I will give you a text rephrase in NVC Language. Be careful to distinguish pseudofeelings from feelings. Rephrase it and then list down those [observations, feelings, needs,  requests] after the rephrased text. here is the original text to rephrase: "'+ message + '". I need each of those keys [original_txt, rephrased_txt, observations, feelings, needs,  requests] in a seperate line - in the format of (key:value) and for each of the these keys [Observations, Feelings, Needs,  Requests] the value must be a valid array format even for single values ex(["val 1", "val 2"], ["val 1"], []) - without gab lines or prefix or postfix response explanation (example valid response: ' "original_txt:\"somevalue\"\nrephrased_txt: \"somevalue.\"\n\nobservations: somevaluesArray\nfeelings: somevaluesArray\nneeds: somevaluesArray\nrequests: somevaluesArray"')'
 
-    result = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=new_prompt,
-        max_tokens=3000,
+    ##########3###### old model
+    # result = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     prompt=new_prompt,
+    #     max_tokens=3000,
+    #     temperature=1.2
+    # )
+    # return {"translation": result["choices"][0]["text"]}
+    ################# new model
+    result = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "user", "content": new_prompt},
+        ],
+        # max_tokens=3000,
         temperature=1.2
     )
     print(result)
-    return {"translation": result["choices"][0]["text"]}
+    return {"translation": result["choices"][0]["message"]["content"]}
 
 
 @app.route('/')
--- static/app.js
+++ static/app.js
@@ -1,6 +1,10 @@
 var translate = document.querySelector("#btn-translate");
 var input_translate = document.querySelector("#input-translate")
 var output_translate = document.querySelector("#output-translate")
+var output_observations = document.querySelector("#output-giraffe-observations")
+var output_feelings = document.querySelector("#output-giraffe-feelings")
+var output_needs = document.querySelector("#output-giraffe-needs")
+var output_requests = document.querySelector("#output-giraffe-requests")
 var loading = document.querySelector("#loading");
 
 // mock server
@@ -12,6 +16,14 @@ var url = "translate"
 
 function urlfunc(url) {
     return url + "?" + "text=" + input_translate.value
+}
+function jsonEncoder(str) {
+  try {
+      let ans = JSON.parse(str);
+      return ans;
+  } catch (e) {
+      return JSON.parse(JSON.parse(JSON.stringify(str)));
+  }
 }
 
 function callback() {
@@ -26,23 +38,46 @@ function callback() {
       })
         .then(response => response.json())
         .then(json => {
-            console.log("json: ", json)
-            var output_text = json[0].translation;
-            
-            output_translate.innerText = output_text.trim(); // For some reason the response comes back with leading \n's
-
-            // Hide spinner
-            loading.style.display = 'none';
-            // Show translate button
-            translate.style.display = 'block';
-
+          let output_text;
+          console.log("original res: ", json)
+          console.log("json0: ", (json[0].translation))
+          //********************* removing new lines and spaces *******************************************/
+          output_text = json[0].translation.split('\n').filter(s => Boolean(s.trim()));
+          console.log("json1: ", output_text)
+          //********************* converting to array of objects (key: [values]) **************************/
+          output_text = output_text.map(e => ( { [`${e.split(':')[0]}`] : jsonEncoder(e.split(':')[1]) } ))
+          console.log("json2: ", output_text)
+          //********************* reducing array of objects to one object *********************************/
+          output_text = output_text.reduce((prev, nxt) => ({...prev, ...nxt}), {})
+          console.log("json3: ", output_text)
+          /*
+            output_text: {
+              "original_txt": "",
+              "rephrased_txt": "",
+              "observations": [],
+              "feelings": [],
+              "needs": [],
+              "requests": []
+            }
+          */
+          //********assigning values */
+          output_translate.innerText = output_text.rephrased_txt; // For some reason the response comes back with leading \n's
+          output_observations.innerText = output_text.observations;
+          output_feelings.innerText = output_text.feelings;
+          output_needs.innerText = output_text.needs;
+          output_requests.innerText = output_text.requests;
+          // Hide spinner
+          loading.style.display = 'none';
+          // Show translate button
+          translate.style.display = 'block';
         }).catch(function errorhandler(error) {
-            // Hide spinner
-            loading.style.display = 'none';
-            // Show translate button
-            translate.style.display = 'block';
+          console.log("err: ", error)
+          // Hide spinner
+          loading.style.display = 'none';
+          // Show translate button
+          translate.style.display = 'block';
 
-            alert("Something wrong with the server. Please try again later.")
+          alert("Something wrong with the server. Please try again later.")
         })
 }
 
